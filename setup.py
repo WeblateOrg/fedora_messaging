153ee37824c895c7a8ce57fa29081196bc365aad
Michal Čihař <michal@cihar.com>
2019-10-11 14:56:29 +0200

    Initial code structure
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2019 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+
+from setuptools import setup
+
+setup(
+    name="weblate_fedora_messaging",
+    version="0.1",
+    packages=["weblate_fedora_messaging"],
+    include_package_data=True,
+    license="MIT",
+    description="Weblate Fedora Messaging integration",
+    long_description="Weblate Fedora Messaging integration",
+    keywords="i18n l10n gettext git mercurial translate",
+    url="https://weblate.org/",
+    author="Michal Čihař",
+    author_email="michal@cihar.com",
+    install_requires=["Weblate"],
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Internationalization",
+        "Topic :: Software Development :: Localization",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
